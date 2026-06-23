1610bf822d4d8846f01f51f95ac5209ca8714979
sandhiya <sandhiyx07@gmail.com>
2026-06-23 17:37:58 +0530

    Completed Strings Part 1
    Added String Basics programs:
    - Input String
    - Count Characters
    - Count Vowels
    - Reverse String
    - Palindrome String

---
+++ count_charac.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    cout << "Number of characters = " << str.length();
+
+    return 0;
+}
---
+++ count_vowels.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    int count = 0;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+        {
+            count++;
+        }
+    }
+
+    cout << "Number of vowels = " << count;
+
+    return 0;
+}
